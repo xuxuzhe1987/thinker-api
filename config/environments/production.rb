6d215d3ee54a332294ff3bb6378ee1e5fee85b3f
xuzhe xu <xuxuzhe@163.com>
2023-07-14 18:43:29 +0800

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.hosts << "www.sker.love"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
